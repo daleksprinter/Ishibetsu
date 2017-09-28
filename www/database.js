201049d3351421a8b8cb0fbea94dc6396c6f94eb
daleksprinter <b1015120@fun.ac.jp>
2017-09-28 17:10:51 +0900

    Project's Initial Commit

---
+++ www/database.js
@@ -0,0 +1,66 @@
+function snapphoto(){
+        
+        navigator.camera.getPicture (onSuccess, onFail, 
+            { quality: 50, destinationType: Camera.DestinationType.DATA_URL,allowEdit:true});
+        
+        function onSuccess (imageData) {
+       
+              var image = document.getElementById ('viewgetphoto');
+                image.src = "data:image/jpeg;base64," + imageData;
+            console.log(image.src);
+        }
+
+        //A callback function when snapping picture is fail.
+        function onFail (message) {
+            alert ('Error occured: ' + message);
+        }
+
+} 
+
+function getphoto(){
+     navigator.camera.getPicture (onSuccess, onFail, 
+            { quality: 50, destinationType: Camera.DestinationType.DATA_URL, sourceType: navigator.camera.PictureSourceType.SAVEDPHOTOALBUM,allowEdit:true});
+        
+        function onSuccess (imageData) {
+       
+              var image = document.getElementById ('viewgetphoto');
+                image.src = "data:image/jpeg;base64," + imageData;
+             console.log(image.src);
+        }
+
+        //A callback function when snapping picture is fail.
+        function onFail (message) {
+            alert ('Error occured: ' + message);
+        }
+}
+
+
+
+function insert(){
+   
+     var db = openDatabase("database", "1.0", "testdatabase", 1000000);
+         
+     var no = document.getElementById("id").value;
+     var title = document.getElementById("title").value;
+     var info = document.getElementById("info").value;
+     var imageurl = document.getElementById("imageurl").value;
+ 
+     db.transaction(
+         function(tr){
+           tr.executeSql('CREATE TABLE IF NOT EXISTS TestTable (id unique, title ,info,mapx,mapy,opentime,endtime,time,season,slope,imageurl)');
+           tr.executeSql('INSERT INTO TestTable VALUES(?,?,?,?,?,?,?,?,?,?)',[no,title,info,imageurl,"","","","","",""]);
+         }
+     );
+    
+}
+
+function init(){
+    
+    var db = openDatabase("database", "1.0", "testdatabase", 1000000);
+    
+    db.transaction(
+        function(tr){
+            tr.executeSql('DROP TABLE IF EXISTS TestTable');
+        }
+    );
+}
---
+++ www/homesettings.js
@@ -0,0 +1,76 @@
+function viewthumbs(){
+     var db = openDatabase("database", "1.0", "testdatabase", 1000000);
+     db.transaction(
+         function(tr){
+             tr.executeSql("SELECT * FROM TestTable",[],function(rt,rs){
+                 var l= rs.rows.length;
+                 for(var i=0;i<l;i++){
+            
+                    var button=createbutton(i,"loadpage(this)","light");
+        
+                    //loadImage
+                     var img = viewimg(button,rs.rows.item(i).imageurl,100,100);
+                     
+                     //setComponent
+                     photo.appendChild(button);
+                     ons.compile(button);
+                 }
+             });
+         });  
+}
+ 
+function createbutton(id,method,modifier){
+     var btn = document.createElement("ons-button");
+                      btn.setAttribute("id",id);
+                      btn.setAttribute("onclick", method);
+                      btn.setAttribute("modifier", modifier);
+                      return btn;
+                     
+}
+
+function refineimg(){
+    var db = openDatabase("database", "1.0", "testdatabase", 1000000);
+    deleteview(photo);
+    
+    //坂判定
+    var slopeflug;
+    if(onslope.isChecked()){
+        slopeflug = 'false';
+    }else{
+        slopeflug = 'true';
+    }
+    
+    
+    //営業判定
+    var nowtime = new Date();
+    var nownum = nowtime.getHours()*100+nowtime.getMinutes();
+    var openflug = null;
+
+  
+   
+    
+    db.transaction(
+         function(tr){
+             tr.executeSql("SELECT * FROM TestTable WHERE slope = ? OR slope = 'false' ",[slopeflug],function(rt,rs){
+                 var l= rs.rows.length;
+        
+                 for(var i=0;i<l;i++){
+
+                    var button=createbutton(i,"loadpage(this)","light");
+        
+                    //loadImage
+                     var img = viewimg(button,rs.rows.item(i).imageurl,100,100);
+                     
+                     //setComponent
+                     photo.appendChild(button);
+                     ons.compile(button);
+                 }
+             });
+         });
+}
+
+function deleteview(id){
+//速度低下のリスク
+ id.textContent = null;  
+ myNavigator.popPage();
+}
