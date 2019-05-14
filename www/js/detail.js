91e704586a28e32dc77403ed1108a10088ff0aa3
daleksprinter <b1015120@fun.ac.jp>
2019-05-14 20:05:52 +0900

    modify loding functions

--- www/js/detail.js
+++ www/js/detail.js
@@ -1,57 +1,57 @@
 var data;//保存用
-function int_to_px(size){
-    return String(size)+'px';
+function int_to_px(size) {
+    return String(size) + 'px';
 }
 
-function load_detail(id){
-     var db = openDatabase("database", "1.0", "testdatabase", 1000000);
-     //スポットデータを読み込む処理。押された写真のIDと一致するタプルを検索
-     db.transaction(
-         function(tr){
-             tr.executeSql("SELECT * FROM Spot INNER JOIN Tag ON Spot.tagid = Tag.id WHERE id = ?",[id],function(rt,rs){
-                 console.log(rs.rows.length);
-                 data = rs.rows.item(0);
-                 
-                 
-                 myNavigator.pushPage('phostone/detail.html');
-             });
-     });   
+function load_detail(id) {
+    var db = openDatabase("database", "1.0", "testdatabase", 1000000);
+    //スポットデータを読み込む処理。押された写真のIDと一致するタプルを検索
+    db.transaction(
+        function (tr) {
+            tr.executeSql("SELECT * FROM Spot INNER JOIN Tag ON Spot.tagid = Tag.id WHERE id = ?", [id], function (rt, rs) {
+                console.log(rs.rows.length);
+                data = rs.rows.item(0);
+
+
+                myNavigator.pushPage('phostone/detail.html');
+            });
+        });
 }
 var latlng;
-document.addEventListener('pageinit',function(page){
-   if(page.target.id == "detail"){
+document.addEventListener('pageinit', function (page) {
+    if (page.target.id == "detail") {
 
-    //画像表示
-    var img = $("#image")[0];
-    img.src = data.imagedata;
-    img.width = screen.width;
-    img.height = screen.width;
-    img.setAttribute('class','view');
-    
-    //テキスト表示
-    $("#title")[0].textContent = data.title;
-    $("#info")[0].textContent = data.info;
-    // $("#opentime")[0].textContent = '開店時間: ' + Number(data.opentime)/100 + '時';
-    // $("#endtime")[0].textContent = '閉店時間: ' + Number(data.endtime)/100+ '時';
+        //画像表示
+        var img = $("#image")[0];
+        img.src = data.imagedata;
+        img.width = screen.width;
+        img.height = screen.width;
+        img.setAttribute('class', 'view');
 
-document.getElementById('detail_text').style.width = int_to_px(screen.width-20);
-document.getElementById('mapcanv').style.width = int_to_px(screen.width);
-document.getElementById('mapcanv').style.height = int_to_px(screen.width);
+        //テキスト表示
+        $("#title")[0].textContent = data.title;
+        $("#info")[0].textContent = data.info;
+        // $("#opentime")[0].textContent = '開店時間: ' + Number(data.opentime)/100 + '時';
+        // $("#endtime")[0].textContent = '閉店時間: ' + Number(data.endtime)/100+ '時';
 
-     latlng = new google.maps.LatLng(data.latitude,data.longitude); 
-    var map = new google.maps.Map(mapcanv, {
-        zoom: 14,       // ズームレベル
-        center: latlng     // 中心地を指定
-      });
-    var marker = new google.maps.Marker({position: latlng, map: map}); 
-    directionsRenderer.setMap(map);
-    //navigator.geolocation.getCurrentPosition(view_root,errorFunc,optionObj);
-    
-   } 
+        document.getElementById('detail_text').style.width = int_to_px(screen.width - 20);
+        document.getElementById('mapcanv').style.width = int_to_px(screen.width);
+        document.getElementById('mapcanv').style.height = int_to_px(screen.width);
+
+        latlng = new google.maps.LatLng(data.latitude, data.longitude);
+        var map = new google.maps.Map(mapcanv, {
+            zoom: 14,       // ズームレベル
+            center: latlng     // 中心地を指定
+        });
+        var marker = new google.maps.Marker({ position: latlng, map: map });
+        directionsRenderer.setMap(map);
+        //navigator.geolocation.getCurrentPosition(view_root,errorFunc,optionObj);
+
+    }
 });
 
 //お気に入り登録処理
-function check_fav(){
+function check_fav() {
     /*s
     if(data.favorite == 'true'){
         console.log('true');
@@ -78,25 +78,25 @@ function check_fav(){
     }*/
 }
 
-function callback(){
+function callback() {
     console.log('suc');
 }
 
-function view_root(position){
-     var start = new google.maps.LatLng(position.coords.latitude,position.coords.longitude); 
-  var request = { 
-        origin:start,
-        destination:latlng,
+function view_root(position) {
+    var start = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var request = {
+        origin: start,
+        destination: latlng,
         travelMode: google.maps.TravelMode.WALKING
-        }; 
+    };
 
-        directionsService.route(request, function(result, status) { 
-            
-              if (status == google.maps.DirectionsStatus.OK) { 
- 
-          
-             directionsRenderer.setDirections(result);
-             } 
-        }); 
+    directionsService.route(request, function (result, status) {
+
+        if (status == google.maps.DirectionsStatus.OK) {
+
+
+            directionsRenderer.setDirections(result);
+        }
+    });
 }
 
--- www/js/home.js
+++ www/js/home.js
@@ -1,47 +1,139 @@
+var db = openDatabase("database", "1.0", "testdatabase", 1000000);
 
-document.addEventListener('pageinit',function(page){
-   if(page.target.id == "home"){
-    
-　　　　$("#photo").empty();//子要素の初期化
-        var db = openDatabase("database", "1.0", "testdatabase", 1000000);
-        db.transaction(
-         function(tr){
-             tr.executeSql("SELECT id,imagedata FROM Spot",[],function(rt,rs){
-                 var l= rs.rows.length;
+function load_database() {
+    //テーブル初期化
+    delete_table();
 
-                 var hero = Math.floor( Math.random() * (l + 1) );
-                 var pht = img(rs.rows.item(hero).imagedata,screen.width-5,screen.width-5);
-                 pht.setAttribute('class','heroimg');
+    //スポットデータの読み込み
+    var loadspot = false;
+    var loadtag = false;
 
-                 $('#herophoto')[0].appendChild(pht);
-                 var time = new Date();
-                 $('#herotime')[0].textContent=String(time.getFullYear()+'/'+time.getMonth()+'/'+time.getDate()+' '+conv_day(time.getDay())+'.')
-                 
-                 for(var i=0;i<l;i++){
-        
-                  //作ったサムネイル要素を追加
-                 $('#photo')[0].appendChild(create_thumbnail(rs.rows.item(i).id,rs.rows.item(i).imagedata,(screen.width-20)/3,(screen.width-20)/3));
-                 
-                 //$('#photo')[0].appendChild(create_thumbnail(rs.rows.item(i).id,rs.rows.item(i).imagedata,(screen.width-36)/3,(screen.width-36)/3));
-                 }
+    $.ajax({
+        type: "GET",
+        url: "http://18.219.20.170:13334/spot",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (datas, dataType) {
+            for(key in datas) insertSpot(datas[key]);
+            loadspot = true;
+            if(loadspot && loadtag) generate_heroPhoto(), generate_thumbnail();
 
-             });
-        });
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert('Error : ' + errorThrown);
+            $("#XMLHttpRequest").html("XMLHttpRequest : " + XMLHttpRequest.status);
+            $("#textStatus").html("textStatus : " + textStatus);
+            $("#errorThrown").html("errorThrown : " + errorThrown);
+        }
+    });
+
+    //タグデータの読み込み
+    $.ajax({
+        type: "GET",
+        url: "http://18.219.20.170:13334/tag",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (datas, dataType) {
+            for(key in datas) insertTag(datas[key]);
+            loadtag = true;
+            if(loadspot && loadtag) generate_heroPhoto(), generate_thumbnail();
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert('Error : ' + errorThrown);
+            $("#XMLHttpRequest").html("XMLHttpRequest : " + XMLHttpRequest.status);
+            $("#textStatus").html("textStatus : " + textStatus);
+            $("#errorThrown").html("errorThrown : " + errorThrown);
+        }
+    })
+
+}
+
+//WebSQL挿入用関数
+function insertSpot(data) {
+    const createsql = 'CREATE TABLE IF NOT EXISTS Spot (id PRIMARY KEY,title,info,time,season,tagid INTEGER)'
+    const sql = "INSERT INTO Spot VALUES(?,?,?,?,?,?)";
+    db.transaction(
+        function (tr) {
+            tr.executeSql(createsql, [], function () {
+            });
+            tr.executeSql(sql, [data.id, data.title, data.info, data.time, data.season, data.tagid], function (rt, rs) {
+            });
+        }
+    );
+}
+function insertTag(data) {
+    const createsql = 'CREATE TABLE IF NOT EXISTS Tag (id PRIMARY KEY,name,latitude,longitude,opentime INTEGER,endtime INTEGER,slope)';
+    const sql = "INSERT INTO Tag VALUES(?,?,?,?,?,?,?)"
+    db.transaction(
+        function (tr) {
+            tr.executeSql(createsql, [], function () {
+            });
+            tr.executeSql(sql, [data.id, data.name, data.latitude, data.longitude, data.opentime, data.endtime, data.slope]);
+        }
+    );
+}
+function delete_table() {
+    db.transaction(
+        function (tr) {
+            tr.executeSql('DROP TABLE IF EXISTS Spot');
+            tr.executeSql('DROP TABLE IF EXISTS Tag');
+
+        }
+    );
+}
+
+
+document.addEventListener('pageinit', function (page) {
+    if (page.target.id == "home") {
+        load_database();
     }
 });
-function create_thumbnail(id,src,width,height){
+
+function generate_thumbnail(){
+    db.transaction(
+        function (tr) {
+            tr.executeSql("SELECT * FROM Spot", [], function (rt, rs) {
+                var l = rs.rows.length;
+                for(var i = 0; i < l; i++) $('#photo')[0].appendChild(create_thumbnail(rs.rows.item(i).id));
+            });
+        });
+}
+
+function generate_heroPhoto(){
+    const bucketUrl = 'https://s3.us-east-2.amazonaws.com/ishibetsu-images/spot-images/24.jpeg';
+    var pht = img(bucketUrl, screen.width, screen.width);
+    $('#herophoto')[0].appendChild(pht);
+}
+
+function create_thumbnail(id) {
+    const url = 'https://s3.us-east-2.amazonaws.com/ishibetsu-images/spot-images/' + String(id) + '.jpeg';
+    console.log(url);
     var div = document.createElement('div');
-    div.setAttribute('class','thumb');
+    div.setAttribute('class', 'thumb');
     var button = document.createElement('button');
-    button.setAttribute('id',id);
+    button.setAttribute('id', id);
     button.setAttribute('class', 'unselected');
-    button.setAttribute('onclick','load_detail(this.id)');
-    button.appendChild(img(src,width,height));
+    button.setAttribute('onclick', 'load_detail(this.id)');
+    button.appendChild(img(url, (screen.width / 3), (screen.width/3)));
     div.appendChild(button);
     return div;
 };
 
-function img(src,width,height){  
+function heroimgMsg(){
+    var time = new Date();
+    var msg = '石別へようこそ'
+    var date = String(time.getFullYear() + '/' + time.getMonth() + '/' + time.getDate() + ' ' + conv_day(time.getDay()) + '.')
+    
+    return (
+        '<div>' + 
+            '<div>' + msg + '</div>' + 
+            '<div>' + date + '</div>' + 
+        '</div>'
+    );
+
+}
+
+function img(src, width, height) {
     var image = document.createElement('img');
     image.src = src;
     image.width = width;
@@ -49,15 +141,14 @@ function img(src,width,height){
     return image;
 };
 
-function conv_day(day_code){
-	switch(day_code){
-		case 0:return 'Sun';
-		case 1:return 'Mon';
-		case 2:return 'Tue';
-		case 3:return 'Wed';
-		case 4:return 'Thu';
-		case 5:return 'Fri';
-		case 6:return 'Sat';
-
-	}
-}
+function conv_day(day_code) {
+    switch (day_code) {
+        case 0: return 'Sun';
+        case 1: return 'Mon';
+        case 2: return 'Tue';
+        case 3: return 'Wed';
+        case 4: return 'Thu';
+        case 5: return 'Fri';
+        case 6: return 'Sat';
+    }
+};
